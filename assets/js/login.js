4e39d2cd3c8cea4b2c222d7fb86d37232b09d180
dongdanyue <1522420418@qq.com>
2020-07-27 16:39:04 +0800

    初始化

---
+++ assets/js/login.js
@@ -0,0 +1,22 @@
+$(function () {
+    // 点击去注册账号的链接
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide();
+        $('.reg-box').show();
+    });
+    // 点击去登录的链接
+    $('#link_login').on('click', function () {
+        $('.login-box').show();
+        $('.reg-box').hide();
+    });
+    // 从layui中获取form对象
+    var form = layui.form;
+    // 通过form.verify函数自定义校验规则
+    form.verify({
+        // 自定义了一个叫做pwd的校验规则
+        pwd: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ]
+    })
+})
